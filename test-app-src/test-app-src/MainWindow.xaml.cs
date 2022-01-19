1652a002d038d0b6adf884c5a5b888b4e93aac96
Ihor-Ihorevych <77419426+Ihor-Ihorevych@users.noreply.github.com>
2022-01-19 10:56:07 +0200

    release
    First release

---
+++ test-app-src/test-app-src/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Win32;
+using QRCoder;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+
+namespace test_app_src
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Private fields
+        private string _docxFilePath;
+        #endregion
+        #region Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+            _docxFilePath = string.Empty;
+        }
+        #endregion
+        #region Handlers
+        /// <summary>
+        /// "Choose file" button handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Open_File_Button_Handler(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog()
+                {
+                    Filter = "Microsoft word file (*.docx,*.doc)|*.docx;*.doc"
+                };
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    _docxFilePath = openFileDialog.FileName;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error while reading a file");
+            }
+        }
+        /// <summary>
+        /// Handles "Start" button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Start_Button_Handler(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_docxFilePath == string.Empty)
+                {
+                    MessageBox.Show("Choose the file");
+                    return;
+                }
+                WordHelper helper = new WordHelper(_docxFilePath);
+                if (helper.AddQrCodes())
+                {
+                    SaveFileDialog saveFileDialog = new SaveFileDialog()
+                    {
+                        Filter = "Microsoft word file (*.docx,*.doc)|*.docx;*.doc"
+                    };
+                    if (saveFileDialog.ShowDialog() == true)
+                    {
+                        if (helper.Save(saveFileDialog.FileName))
+                        {
+                            MessageBox.Show("File saved successfully!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error while saving the file");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error while saving the file");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
---
+++ test-app-src/test-app-src/WordHelper.cs
@@ -0,0 +1,65 @@
+﻿using QRCoder;
+using Syncfusion.DocIO.DLS;
+using System;
+using System.Drawing;
+
+namespace test_app_src
+{
+    public class WordHelper
+    {
+        #region Private fields
+        private WordDocument _wordDocument;
+        private string _filePath;
+        #endregion
+        #region Constructor
+        public WordHelper(string filePath)
+        {
+            _filePath = filePath;
+        }
+        #endregion
+        #region Methods
+        public bool AddQrCodes()
+        {
+            if (_filePath == string.Empty)
+                throw new ArgumentException("Path to .docx file can't be empty");
+            try
+            {
+                _wordDocument = new WordDocument(_filePath);
+                int current_page = 0;
+                int pages_count = _wordDocument.BuiltinDocumentProperties.PageCount;
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                _wordDocument.LastSection.HeadersFooters.LinkToPrevious = false;
+                string userName = Environment.UserName;
+                foreach (WSection section in _wordDocument.Sections)
+                {
+                    WPicture qr = new WPicture(_wordDocument);
+                    QRCodeData qrCodeData = qrGenerator.CreateQrCode($"{userName}, {++current_page} - {pages_count}", QRCodeGenerator.ECCLevel.Q);
+                    QRCode qrCode = new QRCode(qrCodeData);
+                    Bitmap qrCodeImage = qrCode.GetGraphic(100);
+                    qr.LoadImage(qrCodeImage);
+                    qr.Height = 120;
+                    qr.Width = 120;
+                    section.HeadersFooters.Footer.AddParagraph().ChildEntities.Add(qr);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool Save(string path)
+        {
+            try
+            {
+                _wordDocument.Save(path);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
